97766242629c589bdf460e14647e02028cb862a2
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2016-03-06 16:33:34 -0300

    token as an option

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const npmFromGitHub = require('find-npm-by-github');
 const npmUser = require('npm-user');
-const token = 'af9cf3b2d742e7ade074ac310acd30484d5f1980';
 
-module.exports = (github) => {
+module.exports = (github, options) => {
   return new Promise((resolve, reject) => {
-    npmFromGitHub(token, github, (err, npm) => {
+    npmFromGitHub(options.gh, github, (err, npm) => {
       if (err) { reject(err); }
       else if (!npm) { reject(new Error('npm username not found.')); }
       else { resolve(npm); }
